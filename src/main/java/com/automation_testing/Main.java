92e5ec62051b211d2fc7f0ed5e2a6873c8cf5cb1
Ivan Evlantev <ivanevlantev@mail.ru>
2022-04-15 23:49:28 +0400

    Редактирование класса Main.java(удаление строк System.out.println)

--- src/main/java/com/automation_testing/Main.java
+++ src/main/java/com/automation_testing/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         new TestRunClassicBox().run();
-        System.out.println("Test");
-        System.out.println("Commit");
 
 
     }
