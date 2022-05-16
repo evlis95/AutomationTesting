763d6dd6935c0f656dd8630cd606c9461dc581ae
Ivan Evlantev <ivanevlantev@mail.ru>
2022-05-16 19:42:21 +0400

    редактирование класса Post.class

--- src/main/java/com/automation_testing/post_request_pattern/Post.java
+++ src/main/java/com/automation_testing/post_request_pattern/Post.java
@@ -3,18 +3,12 @@ package com.automation_testing.post_request_pattern;
 import com.automation_testing.creatingxml.UniversalRequestRootTag;
 import com.automation_testing.generalsettings.Settings;
 import com.automation_testing.interfaces.Runnable;
-import com.automation_testing.jaxbsettings.CustomCharacterEscapeHandler;
 import com.automation_testing.parsingxml.UniversalResponseRootTag;
+import com.automation_testing.utils.JAXBUtils;
 import com.automation_testing.utils.PrintDataInLogUtils;
-import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 import java.net.InetSocketAddress;
 import java.net.ProxySelector;
 import java.net.URI;
@@ -32,11 +26,6 @@ public abstract class Post implements Runnable {
     public static HttpHeaders headers;
     public static StringBuilder rtsRequest = new StringBuilder();
     public static StringBuilder cookies = new StringBuilder();
-    private final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .proxy(ProxySelector.of(new InetSocketAddress(Settings.HOST_PROXY, Settings.PORT_PROXY)))
-            .connectTimeout(Duration.ofSeconds(10))
-            .build();
     protected Integer codeStatusResponse;
 
     public static void addCookies(String cookie) {
@@ -52,21 +41,19 @@ public abstract class Post implements Runnable {
     }
 
     protected void marshalling(UniversalRequestRootTag rootTag) throws JAXBException {
-        JAXBContext jcCreate = JAXBContext.newInstance(UniversalRequestRootTag.class);
-        Marshaller marshaller = jcCreate.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-        marshaller.setProperty("com.sun.xml.bind.xmlHeaders", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-        marshaller.setProperty(CharacterEscapeHandler.class.getName(), new CustomCharacterEscapeHandler());
-        marshaller.marshal(rootTag, new File(PATH_REQUEST_BODY));
+        JAXBUtils.marshalling(rootTag);
     }
 
     protected UniversalResponseRootTag unmarshalling() throws JAXBException {
-        JAXBContext jcParse = JAXBContext.newInstance(UniversalResponseRootTag.class);
-        Unmarshaller unmarshall = jcParse.createUnmarshaller();
-        return (UniversalResponseRootTag) unmarshall.unmarshal(new StringReader(bodyResponse));
+        return JAXBUtils.unmarshalling();
     }
 
     protected void executingRequest() throws IOException, InterruptedException {
+        HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .proxy(ProxySelector.of(new InetSocketAddress(Settings.HOST_PROXY, Settings.PORT_PROXY)))
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
         HttpRequest request = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofFile(Path.of(PATH_REQUEST_BODY)))
                 .uri(URI.create(Settings.URI_POST))
@@ -81,7 +68,6 @@ public abstract class Post implements Runnable {
                 .setHeader("DeviceId", "097e2a18390d4111")
                 .setHeader("charset", "utf-8")
                 .build();
-
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
         headers = response.headers();
         bodyResponse = response.body();
